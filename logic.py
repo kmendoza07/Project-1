935c57957418740b2f8442c1ce9f95c230b1e371
kmendoza07 <kimmendoza@unomaha.edu>
2023-12-08 20:39:07 -0600

    Updated gui and logic files

--- logic.py
+++ logic.py
@@ -4,21 +4,38 @@ from bank_historyGUI import *
 
 
 class Logic(QMainWindow, Ui_MainWindow):
-    def __init__(self, balance = 0.00):
+    """
+    A class that describes the functions of the bank app
+    """
+
+    def __init__(self, balance=0.00) -> None:
+        """
+        Method that creates the balance object with its initial value.
+        Also calls to other methods upon an action being detected on an app.
+        """
         super().__init__()
         self.setupUi(self)
 
         self.__account_balance = balance
         self.set_balance(self.__account_balance)
+
         self.deposit_Button.clicked.connect(lambda: self.deposit())
         self.withdraw_Button.clicked.connect(lambda: self.withdraw())
         self.history_Button.clicked.connect(lambda: self.view_history())
 
-
-    def set_balance(self, balance):
+    def set_balance(self, balance) -> None:
+        """
+        Method that shows the total account balance on the main page
+        :param balance: total balance
+        :type balance: float
+        """
         self.accountBalance_Label.setText(f'${balance:.2f}')
 
-    def deposit(self):
+    def deposit(self) -> None:
+        """
+        Method that calculates the total balance by adding.
+        Updates the text file upon completing each calculation.
+        """
         with open('Transaction_History.txt', 'a') as transaction:
             try:
                 amount = float(self.input_deposit_amount.text())
@@ -36,7 +53,11 @@ class Logic(QMainWindow, Ui_MainWindow):
             except TypeError:
                 self.message_label.setText(f'Error: Please enter a positive value')
 
-    def withdraw(self):
+    def withdraw(self) -> None:
+        """
+        Method that calculates the total balance by subtracting.
+        Updates the text file upon completing each calculation.
+        """
         with open('Transaction_History.txt', 'a') as transaction:
             try:
                 amount = float(self.input_withdraw_amount.text())
@@ -56,7 +77,11 @@ class Logic(QMainWindow, Ui_MainWindow):
             except TypeError:
                 self.message_label.setText(f'Error: Please enter a positive value')
 
-    def view_history(self):
+    def view_history(self) -> None:
+        """
+        Method that opens a new window upon clicking the button.
+        Displays the text file onto a plain text box.
+        """
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_TransactionWindow()
         self.ui.setupUi(self.window)
@@ -65,4 +90,3 @@ class Logic(QMainWindow, Ui_MainWindow):
         with open('Transaction_History.txt', 'r') as transaction:
             for i in transaction:
                 self.ui.transaction_history_text.insertPlainText(i)
-
